f7a5cff78db80dc7722c6b2c3a60260dab5cd585
Illia <illia2143@gmail.com>
2020-08-10 16:41:20 -0400

    Updates

---
+++ precache-manifest.0a8adb1e2be9e61aca95102bb9c9f864.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a570229669215e47e7185a95fe117a7",
+    "url": "/projectFive/index.html"
+  },
+  {
+    "revision": "6f1ed86d6bde75ab8e76",
+    "url": "/projectFive/static/css/main.c9f96729.chunk.css"
+  },
+  {
+    "revision": "a54554c63a0bdfeacf54",
+    "url": "/projectFive/static/js/2.b3f82dc7.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/projectFive/static/js/2.b3f82dc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f1ed86d6bde75ab8e76",
+    "url": "/projectFive/static/js/main.5219c273.chunk.js"
+  },
+  {
+    "revision": "16a32b72270338cdf59a",
+    "url": "/projectFive/static/js/runtime-main.7cd60a97.js"
+  }
+]);
